138cffebff0e22361c02821f2a3579b26bb517f8
busesrkya <Buseenursarikaya@gmail.com>
2026-08-17 23:40:08 +0300

    OOP: inheritance ve constructor ornekleri eklendi

--- 19-oop/oop.py
+++ 19-oop/oop.py
@@ -65,4 +65,127 @@ student2 = Student("Hasan", 28)
  
 student1.introduce()
 student2.introduce()
+
 """
+Creating objects and using classes:
+   - class: template > car
+   - object: think of it as the structure produced from the template (mercedes, audi)
+"""
+
+class kitap:
+    def __init__(self, ad, yazar, sayfa):
+        self.ad = ad
+        self.yazar = yazar
+        self.sayfa = sayfa
+
+    def bilgi_goster(self):
+        print(f"kitap:{self.ad}")
+        print(f"yazar:{self.yazar}")
+        print(f"sayfa:{self.sayfa}")
+
+# creating an object
+kitap1 = kitap("Python programlama", "Kaan", 500)
+
+# accessing attribute values
+print(kitap1.ad)
+print(kitap1.yazar)
+print(kitap1.sayfa)
+
+# Method
+kitap1.bilgi_goster()
+
+# kitap:Python programlama
+# yazar:Kaan
+# sayfa:500
+
+# Creating multiple objects
+kitap1 = kitap("Python programlama", "kaan", 500)
+kitap2 = kitap("Python programlamaya giriş", "can", 150)
+
+print(kitap2.ad)
+kitap2.bilgi_goster()
+
+
+# Object Oriented Programming
+# constructor special method:
+
+class Person:
+    def __init__(self):
+        print("Wow! it worked automaticly")
+
+p1 = Person()
+
+# (__str__) dunder (magic special method)
+class Person:
+    def __init__(self, n, a):
+        self.name = n
+        self.age = a
+
+    def __str__(self):
+        return f"Name:{self.name}\nAge:{self.age}"
+
+p1 = Person("Elif Yalın", 34)
+print(p1)
+
+# (__repr__) dunder special method: used to get more detailed info for debugging
+class Person:
+    def __init__(self, n, a):
+        self.name = n
+        self.age = a
+
+    def __repr__(self):
+        return f"Name:{self.name!r}\nAge:{self.age!r}"
+
+p1 = Person("Cansu Öztekin", 20)
+print(p1)
+
+
+# Creating our own custom method:
+class Person:
+    def __init__(self, n, a):
+        self.name = n
+        self.age = a
+
+    def myinfo(self):
+        print(f"Hello my name is {self.name}")
+        print(f"I am {self.age} year old")
+
+p1 = Person("Mehmet Demir", 29)
+p1.myinfo()  # functions don't run unless called. The exception to this is def __init__
+
+# Changing a class variable from outside the class:
+class Person:
+    def __init__(self, n, a):
+        self.name = n
+        self.age = a
+
+    def myInfo(self):
+        print(f"Hello my name is {self.name}")
+        print(f"I am {self.age} years old")
+
+p1 = Person("Tuğçe Özgün", 45)
+p1.age = 33
+print(p1.age)
+p1.myInfo()
+
+# We can't leave a class body empty, to avoid an error: use pass or ...
+class Person:
+    pass
+
+# Inheritance
+class Person:
+    def __init__(self, fname, lname):
+        self.firstname = fname
+        self.lastname = lname
+
+    def myPrint(self):
+        print(self.firstname, self.lastname)
+
+p1 = Person("Yılmaz", "Tekin")
+p1.myPrint()
+
+class Student(Person):  # child class (derived class)
+    pass
+
+p1 = Person("Yılmaz", "Tekin")
+p1.myPrint()
